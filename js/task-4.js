cdb5c71e8cf60edf23ad67f535f6a48a4d5b1deb
StanislavBondarenko631 <skyritm201287@gmail.com>
2026-06-26 17:31:10 +0300

    finish hw 7 mod js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailValue = event.currentTarget.elements.email.value.trim();
+  const passwordValue = event.currentTarget.elements.password.value.trim();
 
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+  console.log(formData);
+  event.currentTarget.reset();
+});
--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,11 @@
+const buttonChangeColor = document.querySelector(".change-color");
+const textSpan = document.querySelector(".color");
+
+buttonChangeColor.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  textSpan.textContent = newColor;
+});
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
